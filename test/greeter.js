9593543ab93d294507e54438def940c196899644
DJ Carter <dj.carter@bcgdv.com>
2017-05-09 17:10:36 -0700

    Add test to kill a greeter

--- test/greeter.js
+++ test/greeter.js
@@ -5,9 +5,15 @@ contract ('greeter', function(accounts) {
     return Greeter.deployed().then(function(instance) {
       return instance.greet.call();
     }).then(function(greeting) {
-      assert.equal(greeting.valueOf(), "Hello Mr. Fish!", "Incorrect greeting returned");
+      assert.equal(greeting.valueOf(), "Hello Mr. Fish!", "incorrect greeting returned");
     });
   });
 
-
+  it("should be able to kill a greeter", function() {
+    return Greeter.deployed().then(function(instance) {
+      return instance.kill.call();
+    }).then(function(result) {
+      assert.equal(result.valueOf(), "", "unable to kill the greeter");
+    });
+  });
 });
